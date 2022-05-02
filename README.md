8b76ded10548a24a74f330955b0cbd53058bcec5
ERSİN ÇÖL <ersincol235@gmail.com>
2022-05-02 15:41:20 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# QUEUE
+
+* Bilginin geliş sırasına göre ilk gelen elemana ilk erişilen liste yapısıdır.
+* FIFO first in first out
+* Analoji yapacak olursak sinemaya bilet almaya gelen kişilerde sıradaki ilk kişi ilk biletini  alır ve sıradan çıkar
+* enqueue eleman ekleme, dequeue eleman çıkarma, peek sıradaki elemanı getir, rear kuyrugun arkasını gösteren işaretçi front kuyruğun önünü gösteren işaretçi
+* priority queue: öncelikli kuyruk seneryosu mesela hastanedeki kuyruk için bazı kişiler öncelikli olabilir anolojisini düşünebiliriz
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)  {
+        // queue linked list ile kullanımı
+        Queue<Integer> q=new LinkedList();
+        for(int i=0;i<5;i++){
+            q.add(i);
+        }
+        System.out.println(q);
+        System.out.println("removed element: "+q.remove());
+        System.out.println(q);
+        System.out.println("head of queue: "+q.peek());
+        System.out.println("size of queue: "+q.size());
+        // queue priorityqueue ile kullanımı
+        Queue<String> pq=new PriorityQueue<>();
+        pq.add("İstanbul");
+        pq.add("Ankara");
+        pq.add("Samsun");
+        pq.add("Rize");
+        System.out.println(pq);
+        pq.remove("Samsun");
+        System.out.println("After remove: "+pq);
+        System.out.println("After pool: "+pq.poll());
+        System.out.println("Final queue: "+pq);
     }
 }
